0f2a5918e92d0bdf6d07706276405eca1fedb6e4
Ubuntu <ubuntu@ip-10-194-31-48.ec2.internal>
2013-03-19 07:39:09 +0000

    Alter form.php

--- packages/kapow/elements/system/captcha/kapow/form.php
+++ packages/kapow/elements/system/captcha/kapow/form.php
@@ -3,26 +3,19 @@
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
 $form = Loader::helper('form');
-$pkg = Package::getByHandle('recaptcha');
+$pkg = Package::getByHandle('kapow');
 ?>
 
 <div class="clearfix">
-	<?=$form->label('CAPTCHA_RECAPTCHA_PUBLIC_KEY', t('Public Key'))?>
+	<?=$form->label('KAPOW_PUBLIC_KEY', t('Public Key'))?>
 	<div class="input">
-		<?=$form->text('CAPTCHA_RECAPTCHA_PUBLIC_KEY', $pkg->config('CAPTCHA_RECAPTCHA_PUBLIC_KEY'), array('class' => 'span6'))?>
+		<?=$form->text('KAPOW_PUBLIC_KEY', $pkg->config('KAPOW_PUBLIC_KEY'), array('class' => 'span6'))?>
 	</div>
 </div>
 
 <div class="clearfix">
-	<?=$form->label('CAPTCHA_RECAPTCHA_PRIVATE_KEY', t('Private Key'))?>
+	<?=$form->label('KAPOW_PRIVATE_KEY', t('Private Key'))?>
 	<div class="input">
-		<?=$form->text('CAPTCHA_RECAPTCHA_PRIVATE_KEY', $pkg->config('CAPTCHA_RECAPTCHA_PRIVATE_KEY'), array('class' => 'span6'))?>
-	</div>
-</div>
-
-<div class="clearfix">
-	<?=$form->label('CAPTCHA_RECAPTCHA_THEME', t('Theme'))?>
-	<div class="input">
-		<?=$form->select('CAPTCHA_RECAPTCHA_THEME', array('red' => t('red (default theme)'), 'white' => t('white'), 'blackglass' => t('blackglass'), 'clean' => t('clean')), $pkg->config('CAPTCHA_RECAPTCHA_THEME'))?>
+		<?=$form->text('KAPOW_PRIVATE_KEY', $pkg->config('KAPOW_PRIVATE_KEY'), array('class' => 'span6'))?>
 	</div>
 </div>
