28c36eac88c5ca356279f8b0da51196a91a8992e
Ubuntu <ubuntu@ip-10-194-31-48.ec2.internal>
2013-03-19 07:56:09 +0000

    Use Loader not require.

--- packages/kapow/tools/process_url.php
+++ packages/kapow/tools/process_url.php
@@ -4,8 +4,8 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
 
 if(isset($_POST['kapow_comment']))
 {
-     include_once "headwinds2lib.php";
-     $response = _headwinds2_return_initial_cookie($_POST['kapow_comment'],
+    Loader::library('3rdparty/headwinds2lib', 'kapow');
+    $response = _headwinds2_return_initial_cookie($_POST['kapow_comment'],
                             $_POST['kapow_author'], $_POST['kapow_email']);
     echo json_encode($response);
 }
